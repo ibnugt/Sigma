8530e3ace778802f0d09ecbfdecf82f003ff82ac
ibnugt <ibnumuzakim7@gmail.com>
2025-07-12 06:33:18 +0700

    Delete PyroUbot/modules directory

--- PyroUbot/modules/prefix.py
+++
@@ -1,7 +0,0 @@
-__MODULE__ = "ᴘʀᴇғɪx"
-__HELP__ = """
-<blockquote><b>Bantuan untuk prefix
-
-perintah : <code>{0}prefix</code> [simbol]
-    untuk merubah prefix/handler perintah, tutor none prefix atau tanpa prefix: <a href=https://t.me/InfoKingzUserbot/51>ᴄʟɪᴄᴋ ᴋɪɴɢᴢ</a> </b></blockquote>
-"""
--- PyroUbot/modules/tictactoe.py
+++
@@ -1,9 +0,0 @@
-from PyroUbot import *
-
-__MODULE__ = "ᴛɪᴄᴛᴀᴄᴛᴏᴇ"
-__HELP__ = """
-<blockquote>Bantuan Untuk Tictactoe
-
-perintah : <code>{0}tictactoe</code>
-   untuk memunculkan game tictactoe</blockquote>
-"""

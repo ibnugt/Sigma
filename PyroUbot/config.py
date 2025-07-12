7337344a4ce947103b887dfbb260d3e938d7302d
ibnugt <ibnumuzakim7@gmail.com>
2025-07-12 19:52:38 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -7,9 +7,9 @@ MAX_BOT = int(os.getenv("MAX_BOT", "100"))
 
 DEVS = list(map(int, os.getenv("DEVS", "1496823926").split()))
 
-API_ID = int(os.getenv("API_ID", "25805438"))
+API_ID = int(os.getenv("API_ID", "28914736"))
 
-API_HASH = os.getenv("API_HASH", "a47c79aa127d0214ceb4cc7aaab578c6")
+API_HASH = os.getenv("API_HASH", "305e0434d99b9bdbe98c8e7bec306193")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7312972618:AAEWIMr7UV9-mIqA2UXulD8A5r22T2MX03E")
 
